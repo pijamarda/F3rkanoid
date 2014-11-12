f61eea5fe23f49e5e02c1bdbd2e51c2e368719fd
pijamarda <zeneke@gmail.com>
2014-11-12 13:14:58 +0100

    gitignore y añadido fichero de requerimientos

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 rm *.o
 
-g++ -c -std=c++0x *.cpp -I /home/zeneke/data/sdk/sfml-2.1/include
-g++ *.o -o sfml-app -L /home/zeneke/data/sdk/sfml-2.1/lib -lsfml-graphics -lsfml-window -lsfml-system
-#export LD_LIBRARY_PATH=/home/zeneke/data/sdk/sfml-2.1/lib && ./sfml-app
+g++ -c -std=c++0x *.cpp -I /home/fgimenez/data/sdk/sfml-2.1/include
+g++ *.o -o sfml-app -L /home/fgimenez/data/sdk/sfml-2.1/lib -lsfml-graphics -lsfml-window -lsfml-system
+#export LD_LIBRARY_PATH=/home/fgimenez/data/sdk/sfml-2.1/lib && ./sfml-app
