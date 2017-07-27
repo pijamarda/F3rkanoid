09e26b5833c219f6fa50d55e3c07f910ae02ea19
zeneke <zeneke@gmail.com>
2017-07-27 11:30:18 +0200

    fixed script to compile/link/run

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-rm *.o
+rm out/*.o
 
-g++ -c -std=c++0x *.cpp -I /home/fgimenez/data/sdk/sfml-2.1/include
-g++ *.o -o sfml-app -L /home/fgimenez/data/sdk/sfml-2.1/lib -lsfml-graphics -lsfml-window -lsfml-system
+g++ -c -std=c++0x source/*.cpp
+mv *.o out/
+g++ out/*.o -o out/f3rkanoid -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
 #export LD_LIBRARY_PATH=/home/fgimenez/data/sdk/sfml-2.1/lib && ./sfml-app
